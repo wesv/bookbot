6a290b97c507c17b8fae7400ae0a060460bdf265
wesv <wesley.vansteenburg@gmail.com>
2025-06-10 13:43:42 -0500

    refactored funxction into it's own file

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
-def num_words(str):
-	return  len(str.split())
+from stats import get_num_words
 
 def get_book_text(file_path):
 	with open(file_path) as f:
 		return f.read()
 
 def main():
-	print(f'{num_words(get_book_text("books/frankenstein.txt"))} words found in the document')
+	print(f'{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document')
 	#print(num_words("Hello my test"))
 
 if __name__ == "__main__":
