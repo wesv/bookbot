deddfdad5f443abcc2b9929767deb7aaa458af65
wesv <wesley.vansteenburg@gmail.com>
2025-06-11 22:08:40 -0500

    added frequency function

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 from stats import get_num_words
+from stats import get_character_frequency
 
 def get_book_text(file_path):
 	with open(file_path) as f:
 		return f.read()
 
 def main():
-	print(f'{get_num_words(get_book_text("books/frankenstein.txt"))} words found in the document')
-	#print(num_words("Hello my test"))
+	text = get_book_text("books/frankenstein.txt")
+	print(f'{get_num_words(text)} words found in the document')
+	char_frequency = get_character_frequency(text)
+	print(f'{char_frequency}')
 
 if __name__ == "__main__":
 	main()
--- stats.py
+++ stats.py
@@ -1,2 +1,12 @@
 def get_num_words(str):
         return  len(str.split())
+
+def get_character_frequency(text):
+	dict = {}
+	for char in text:
+		char = char.lower()
+		if char not in dict:
+			dict[char] = 1
+		else:
+			dict[char] = dict[char] + 1
+	return dict
